002d4be7f4ba94e9b9870db97db13aca865c7ca4
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-03-10 17:35:42 +0530

    2540

--- Day2.py
+++ Day2.py
@@ -1,8 +1,10 @@
 #2540. Minimum Common Value
+
 def getCommon(self,nums1,nums2):
   nums1_set=set(nums1)
   for i in nums2:
     if i in nums1_set:
       return i
   return -1
+  
 getCommon([1,4,7,9],[3,7,8])
