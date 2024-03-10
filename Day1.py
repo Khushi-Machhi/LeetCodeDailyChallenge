cea4e463cc33078fdbe33f589f1097aab7ee38f0
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-03-10 17:33:07 +0530

    Day1.py

---
+++ Day1.py
@@ -0,0 +1,15 @@
+# 3005.Count Element With Maximum Frequency
+
+from collections import Counter
+def maxFrequencyElement(nums):
+    
+    f=Counter(nums)
+    freq = [i for i in f.values()]
+    m=max(freq)
+    c=0
+    for i in freq:
+        if i==m:
+            c=c+i 
+    return c
+  
+maxFrequencyElement([1,2,5,2,1,2])
