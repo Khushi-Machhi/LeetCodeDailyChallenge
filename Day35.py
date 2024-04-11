ddbc23a47e4dbef9be58d8207c9515cb8ce12088
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-04-11 11:24:29 +0530

    402

---
+++ Day35.py
@@ -0,0 +1,27 @@
+# 402. Remove K Digits
+
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+      
+        stack = []
+        for digit in num:
+            while k > 0 and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        while k > 0:
+            stack.pop()
+            k -= 1
+
+        while stack and stack[0] == '0':
+            stack.pop(0)
+
+        if not stack:
+            return '0'
+        return ''.join(stack)
