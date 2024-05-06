32548bbed4a25431080d18bd5f99f25885a7e9b8
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-05-06 19:34:26 +0530

    2487

---
+++ Day60.py
@@ -0,0 +1,32 @@
+# 2487. Remove Nodes From Linked List
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        if not head or not head.next:
+            return head
+        
+        stack = []
+        curr = head
+        while curr:
+            while stack and stack[-1].val < curr.val:
+                stack.pop()
+            stack.append(curr)
+            curr = curr.next
+       
+        for i in range(len(stack) - 1):
+            stack[i].next = stack[i+1]
+        
+        stack[-1].next = None  
+        
+        return stack[0]
+        
