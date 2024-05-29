ba246ed6e1018c1ebdf692c57f2072412d5bbdf5
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-05-29 20:26:22 +0530

    1404

---
+++ Day83.py
@@ -0,0 +1,27 @@
+# 1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        steps = 0
+        s = list(s)
+        
+        while len(s) > 1:
+            if s[-1] == '0':
+                s.pop()
+            else:
+                i = len(s) - 1
+                while i >= 0 and s[i] == '1':
+                    s[i] = '0'
+                    i -= 1
+                if i >= 0:
+                    s[i] = '1'
+                else:
+                    s.insert(0, '1')
+            steps += 1
+        
+        return steps
+        
