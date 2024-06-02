52ad756c87190e3d829939b18042675cb054369c
Khushi Machhi <156450730+Khushi-Machhi@users.noreply.github.com>
2024-06-02 18:51:57 +0530

    3110

---
+++ Day86.py
@@ -0,0 +1,16 @@
+# 3110. Score of a String
+
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+        return score
+
+        
+        
